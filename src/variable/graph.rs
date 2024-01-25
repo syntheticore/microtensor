2d739f00ee2de140af5f7431cfb0d8d53564520d
Björn Breitgoff <syntheticore@users.noreply.github.com>
2024-01-25 18:41:05 +0100

    Slim down dumps

--- src/variable/graph.rs
+++ src/variable/graph.rs
@@ -7,6 +7,7 @@ use serde::{Serialize, Deserialize, de::DeserializeOwned};
 
 use crate::{
   internal::*,
+  ops::BaseOps,
   scalar::Real,
   variable::{ Variable, Node, NodeCell, Op },
   Tensor,
@@ -57,15 +58,18 @@ impl<T: Real + Serialize + DeserializeOwned + 'static> Graph<T> {
     for dump in tensor_dump.history {
       let node = Node {
         id: dump.id,
-        cell: dump.cell.clone(),
+        cell: NodeCell {
+          data: dump.data.complete(),
+          grad: dump.grad.and_then(|grad| Some(grad.detach()) )
+        },
         op: dump.op,
         previous: dump.previous.iter().map(|id| nodes[id].clone() ).collect(),
         trainable: dump.trainable,
       };
       nodes.insert(node.id, RcT::new(node));
     }
-    let map_tensor = |dump: VariableDump| Variable {
-      node: nodes[&dump.node].clone(),
+    let map_tensor = |dump: usize| Variable {
+      node: nodes[&dump].clone(),
     };
     Ok(Graph {
       inputs: tensor_dump.inputs.into_iter().map(map_tensor).collect(),
@@ -79,21 +83,17 @@ impl<T: Real + Serialize + DeserializeOwned + 'static> Graph<T> {
     let history_dump = history.iter().map(|node| {
       let op: Vec<u8> = postcard::to_allocvec(&node.op).unwrap();
       let op = postcard::from_bytes(&op).unwrap();
-      let mut cell = node.cell.clone();
-      cell.data = cell.data.detach();
-      cell.grad = cell.grad.and_then(|grad| Some(grad.detach()) );
       NodeDump {
         id: node.id,
-        cell,
+        data: if node.trainable { node.cell.data.detach() } else { Tensor::scalar(T::zero()).broadcast(node.cell.data.shape(), None) },
+        grad: node.cell.grad.as_ref().and_then(|grad| Some(Tensor::scalar(T::zero()).broadcast(grad.shape(), None)) ),
         op,
         previous: node.previous.iter().map(|prev| prev.id ).collect(),
         trainable: node.trainable,
       }
     }).collect();
 
-    let map_tensor = |tensor: &Variable<T>| VariableDump {
-      node: tensor.node.id,
-    };
+    let map_tensor = |tensor: &Variable<T>| tensor.node.id;
 
     let graph_dump = GraphDump {
       history: history_dump,
@@ -106,24 +106,19 @@ impl<T: Real + Serialize + DeserializeOwned + 'static> Graph<T> {
   }
 }
 
-
 #[derive(Serialize, Deserialize)]
 struct NodeDump<T: Real + 'static> {
   id: usize,
-  cell: NodeCell<T>, //XXX don't save gradients
+  data: Tensor<T>,
+  grad: Option<Tensor<T>>,
   op: Option<Op<T>>,
   previous: Vec<usize>,
   trainable: bool,
 }
 
 #[derive(Serialize, Deserialize)]
-struct VariableDump {
-  node: usize,
-}
-
-#[derive(Serialize, Deserialize)]
 struct GraphDump<T: Real + 'static> {
   history: Vec<NodeDump<T>>,
-  inputs: Vec<VariableDump>,
-  outputs: Vec<VariableDump>,
+  inputs: Vec<usize>,
+  outputs: Vec<usize>,
 }
